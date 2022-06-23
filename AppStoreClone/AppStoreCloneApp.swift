9d96472803388e268efd4b4b1b34584f837732b5
namgeon1106 <namgeon1106@naver.com>
2022-06-23 14:50:26 +0900

    Initial Commit

---
+++ AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by 김남건 on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
