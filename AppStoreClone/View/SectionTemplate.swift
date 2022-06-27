fa05b97fe35004df5e77e3fd4a84c321516a6616
namgeon1106 <namgeon1106@naver.com>
2022-06-27 22:34:34 +0900

    fix: Section Template 사용 시 Navigation bar title 안보이는 버그 수정

--- AppStoreClone/View/SectionTemplate.swift
+++ AppStoreClone/View/SectionTemplate.swift
@@ -14,46 +14,46 @@ struct SectionTemplate<Content: View>: View {
     let content: () -> Content
 
     var body: some View {
-        VStack(spacing: 12) {
-            Divider()
-                .padding(.horizontal, Constants.horizontalMargin)
-            if let title = title {
-                VStack(alignment: .leading, spacing: 6) {
-                    HStack {
-                        Text(title)
-                            .font(.title3)
-                            .fontWeight(.bold)
-                        Spacer()
-                        if hasSeeAllButton {
-                            NavigationLink("모두 보기") {
-                                Text("Hello World!")
+        NavigationView {
+            VStack(spacing: 12) {
+                Divider()
+                    .padding(.horizontal, Constants.horizontalMargin)
+                if let title = title {
+                    VStack(alignment: .leading, spacing: 6) {
+                        HStack {
+                            Text(title)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Spacer(minLength: 0)
+                            if hasSeeAllButton {
+                                NavigationLink("모두 보기") {
+                                    Text("Hello World!")
+                                }
                             }
                         }
-                    }
 
-                    if let subTitle = subTitle {
-                        Text(subTitle)
-                            .font(.caption)
-                            .foregroundColor(Color(.lightGray))
-                            .padding(.bottom, 3)
+                        if let subTitle = subTitle {
+                            Text(subTitle)
+                                .font(.caption)
+                                .foregroundColor(Color(.lightGray))
+                                .padding(.bottom, 3)
+                        }
                     }
+                    .padding(.horizontal, Constants.horizontalMargin)
                 }
-                .padding(.horizontal, Constants.horizontalMargin)
+                content()
             }
-            content()
+            .navigationBarHidden(true)
         }
-        .navigationBarHidden(true)
     }
 }
 
 struct SectionTemplate_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            SectionTemplate(title: nil,
-                            subTitle: nil,
-                            hasSeeAllButton: false) {
-                TopCardListView()
-            }
+        SectionTemplate(title: nil,
+                        subTitle: nil,
+                        hasSeeAllButton: false) {
+            TopCardListView()
         }
         .preferredColorScheme(.dark)
     }
