640ee71aafdbfabd7f89261168e0febe09ab537e
namgeon1106 <namgeon1106@naver.com>
2022-06-27 22:41:00 +0900

    fix: SectionTemplate에서 NavigationView 제거

--- AppStoreClone/View/SectionTemplate.swift
+++ AppStoreClone/View/SectionTemplate.swift
@@ -8,52 +8,46 @@
 import SwiftUI
 
 struct SectionTemplate<Content: View>: View {
-    let title: String?
+    let title: String
     let subTitle: String?
     let hasSeeAllButton: Bool
     let content: () -> Content
 
     var body: some View {
-        NavigationView {
-            VStack(spacing: 12) {
-                Divider()
-                    .padding(.horizontal, Constants.horizontalMargin)
-                if let title = title {
-                    VStack(alignment: .leading, spacing: 6) {
-                        HStack {
-                            Text(title)
-                                .font(.title3)
-                                .fontWeight(.bold)
-                            Spacer(minLength: 0)
-                            if hasSeeAllButton {
-                                NavigationLink("모두 보기") {
-                                    Text("Hello World!")
-                                }
-                            }
-                        }
-
-                        if let subTitle = subTitle {
-                            Text(subTitle)
-                                .font(.caption)
-                                .foregroundColor(Color(.lightGray))
-                                .padding(.bottom, 3)
+        VStack(spacing: 12) {
+            Divider()
+                .padding(.horizontal, Constants.horizontalMargin)
+            VStack(alignment: .leading, spacing: 6) {
+                HStack {
+                    Text(title)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Spacer(minLength: 0)
+                    if hasSeeAllButton {
+                        Button("모두 보기") {
                         }
                     }
-                    .padding(.horizontal, Constants.horizontalMargin)
                 }
-                content()
+
+                if let subTitle = subTitle {
+                    Text(subTitle)
+                        .font(.caption)
+                        .foregroundColor(Color(.lightGray))
+                        .padding(.bottom, 3)
+                }
             }
-            .navigationBarHidden(true)
+            .padding(.horizontal, Constants.horizontalMargin)
+            content()
         }
     }
 }
 
 struct SectionTemplate_Previews: PreviewProvider {
     static var previews: some View {
-        SectionTemplate(title: nil,
+        SectionTemplate(title: "무료 앱 순위",
                         subTitle: nil,
-                        hasSeeAllButton: false) {
-            TopCardListView()
+                        hasSeeAllButton: true) {
+            AppInfoListView(appsCount: 15, ranked: true, rowSize: .small)
         }
         .preferredColorScheme(.dark)
     }
